faa58298b13047c99ddc87e0c146a715930da070
ayazhan8 <ayerikkyzy@gmail.com>
2024-02-07 03:44:07 +0000

    add: projects section

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,5 @@
+import fullStackImg from "@/public/full-stack.png";
+
 export const links = [
   {
     name: "Home",
@@ -22,5 +24,29 @@ export const links = [
   {
     name: "Contact",
     hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Project1",
+    description:
+      "Weather app with AI integration. AI uses data from WeatherAPI to generate a forecast for the day.",
+    tags: ["React", "Next.js", "Tailwind", "Prisma"],
+    imageUrl: fullStackImg,
+  },
+  {
+    title: "Project2",
+    description:
+      "Personal blog website. It has features like filtering and sorting posts and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: fullStackImg,
+  },
+  {
+    title: "Project3",
+    description:
+      "Full-stack e-commerce app with features like cart shopping, filtering, sorting, searching for products, etc.",
+    tags: ["React", "Next.js", "Tailwind", "Framer"],
+    imageUrl: fullStackImg,
   },
 ] as const;
