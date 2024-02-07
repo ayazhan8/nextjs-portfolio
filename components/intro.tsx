92971f84fe37b79c046bf8d07ef12552f210dcca
ayazhan8 <ayerikkyzy@gmail.com>
2024-02-07 01:28:31 +0000

    add: intro section

---
+++ components/intro.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import React from 'react'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
+
+
+
+export default function intro() {
+  return (
+    <section className='mb-28 max-2-[50rem] text-center sm:mb-0'>
+        <div className='flex items-center justify-center'>
+            <div className='relative'>
+                <motion.div
+                    initial={{ opacity: 0, scale: 0}}
+                    animate={{ opacity: 1, scale: 1}}
+                    transition={{
+                        type: "tween",
+                        duration: 0.15
+                    }}
+                    >
+                <Image 
+                    src="/ayazhan.jpeg" 
+                    alt="" 
+                    width="192"
+                    height="192"
+                    quality="95"
+                    priority={true}
+                    className='h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl'
+                />
+                </motion.div>
+                <motion.span 
+                    className='absolute bottom-0 right-0 text-4xl'
+                    initial={{ opacity: 0, scale: 0}}
+                    animate={{ opacity: 1, scale: 1}}
+                    transition={{
+                        type: "spring",
+                        delay: 0.15,
+                        duration: 0.6
+                    }}
+                    >
+                    👩🏻‍💻
+                </motion.span>
+            </div>
+        </div>
+
+        <motion.p 
+            className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0}}
+            >
+            <span className='font-bold'>
+                Hello, I'm Ayazhan.
+            </span>
+            {` I'm a `}
+            <span className='font-bold'>
+                frontend developer
+            </span>
+            {` with a foundation in `}
+            <span className='font-bold'>
+                computer science
+            </span>
+            {` and nearly a year of hands-on experience in a `}
+            <span className="font-bold">
+                dynamic startup environment
+            </span>
+            {`, specialising in `}
+            <span className="font-bold">
+                web application development. 
+            </span>
+        </motion.p>
+
+        <motion.div 
+            className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0}}
+            transition={{
+                delay: 0.1
+            }}
+            >
+            <Link
+                href="#contact"
+                className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+                >
+                    Contact me here <BsArrowRight className='opacity-75 group-hover:translate-x-1 transition'/>
+            </Link>
+
+            <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/7" href="/CV.pdf" download>
+                Download CV <HiDownload className='opacity-70 group-hover:translate-y-1 transition'/>
+            </a>
+
+            <a className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/7"
+                href="https://www.linkedin.com/in/ayerikkyzy/" target="_blank"
+                >
+                <BsLinkedin />
+            </a>
+
+            <a className="bg-white text-gray-700 text-[1.35rem] p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/7"
+                href="https://github.com/ayazhan8" target="_blank" 
+                >
+                <FaGithubSquare />
+            </a>
+        </motion.div>
+    </section>
+  )
+}
