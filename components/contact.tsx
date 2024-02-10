e5bfe574403dbcee9803ef96b1e911c4f1530dd2
ayazhan8 <ayerikkyzy@gmail.com>
2024-02-10 20:32:32 +0000

    fix: loading state on submit button

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "@/components/submit-btn";
 
 export default function contact() {
   const { ref } = useSectionInView("Contact");
@@ -58,16 +58,8 @@ export default function contact() {
           className="h-52 my-3 rounded-lg borderBlack p-4 outline-black"
           placeholder="Your message"
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
 }
-
-// re_9ro4eaJ8_82s9tjmn4JMWYvFrgDBAmJen
